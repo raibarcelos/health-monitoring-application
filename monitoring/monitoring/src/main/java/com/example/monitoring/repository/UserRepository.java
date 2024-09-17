1069a14a7d26762159bf584d5cc39139b4988909
barcelos <rai.barcelos06@gmail.com>
2024-09-17 16:47:32 -0300

    UserRepository

--- monitoring/monitoring/src/main/java/com/example/monitoring/repository/UserRepository.java
+++ monitoring/monitoring/src/main/java/com/example/monitoring/repository/UserRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.monitoring.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    // Método para deletar um usuário por ID
+    void deleteById(Long id);
 }
